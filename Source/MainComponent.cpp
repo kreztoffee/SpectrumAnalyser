9b7a836909225f718a6200f422aafd9db916a124
Chris <bigc123@hotmail.co.uk>
2019-08-31 15:56:27 +0100

    Initial commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,44 @@
+#include "MainComponent.h"
+
+MainComponent::MainComponent()
+{
+    setSize (800, 600);
+
+    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
+        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    {
+        RuntimePermissions::request (RuntimePermissions::recordAudio,
+                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+    }
+    else
+    {
+        setAudioChannels (2, 2);
+    }
+}
+
+MainComponent::~MainComponent()
+{
+    shutdownAudio();
+}
+
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    bufferToFill.clearActiveBufferRegion();
+}
+
+void MainComponent::releaseResources()
+{
+}
+
+void MainComponent::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class MainComponent   : public AudioAppComponent
+{
+public:
+    MainComponent();
+    ~MainComponent();
+
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    void paint (Graphics& g) override;
+    void resized() override;
+
+private:
+	TextButton m_btnOpen, m_btnOpen, m_btnOpen, m_btnOpen;
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
